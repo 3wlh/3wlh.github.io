3f2af0c7f953fe4ccb702843993c68a1fff1fb5a
3wking <43069914+3wking@users.noreply.github.com>
2022-11-08 23:19:25 +0800

    Create remove.sh

---
+++ openwrt/remove.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+opkg remove luci-app-nft-qos --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-adblock --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-nlbwmon --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-watchcat --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-upnp --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-smartdns --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-aria2 --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-hd-idle --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-minidlna --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-samba4 --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-app-sqm --force-removal-of-dependent-packages --autoremove &&
+opkg remove luci-theme-argon &&
+opkg remove luci-theme-material &&
+opkg remove luci-theme-openwrt-2020
