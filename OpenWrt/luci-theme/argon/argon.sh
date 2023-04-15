ea4d8d1456d7df0f8926d77db9063493f90d1f57
3wking <1094890624@qq.com>
2023-04-16 01:39:47 +0800

    更新 argon.sh

--- OpenWrt/luci-theme/argon/argon.sh
+++ OpenWrt/luci-theme/argon/argon.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-argon=" https://raw.iqiq.io/3wking/3wking.github.io/main/OpenWrt/luci-theme/argon/luci-theme-argon_2.2.9-20211016-1_all.ipk"
+argon="https://raw.iqiq.io/3wking/3wking.github.io/main/OpenWrt/luci-theme/argon/luci-theme-argon_2.2.9-20211016-1_all.ipk"
 argon_config="https://raw.iqiq.io/3wking/3wking.github.io/main/OpenWrt/luci-theme/argon/luci-app-argon-config_0.9-20210309_all.ipk"
 install() {
 echo "下载 argon ......" 
