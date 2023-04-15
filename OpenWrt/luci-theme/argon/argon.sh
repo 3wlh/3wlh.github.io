20d95af985e84d84bdb00a196be999445dcefb84
3wking <1094890624@qq.com>
2023-04-16 00:49:28 +0800

    更新 argon.sh

--- OpenWrt/luci-theme/argon/argon.sh
+++ OpenWrt/luci-theme/argon/argon.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 argon=" https://raw.iqiq.io/3wking/3wking.github.io/main/OpenWrt/luci-theme/argon/luci-theme-argon_2.2.9-20211016-1_all.ipk"
 argon_config="https://raw.iqiq.io/3wking/3wking.github.io/main/OpenWrt/luci-theme/argon/luci-app-argon-config_0.9-20210309_all.ipk"
+echo "下载 argon ......" 
+/usr/bin/wget -P /tmp "${argon}" 
 echo "安装 argon ......" 
-wget -P /tmp ${argon} && opkg install luci-theme-argon*.ipk && rm -f /tmp/luci-theme-argon*.ipk
-echo "安装 argon_config ......" 
-wget -P /tmp ${argon_config} && opkg install luci-app-argon-config*.ipk && rm -f /tmp/luci-app-argon-config*.ipk
-}
-echo "argon 安装中 ......" && install && echo "argon 安装完成"
+opkg install luci-theme-argon*.ipk && rm -f /tmp/luci-theme-argon*.ipk && echo "安装 argon 完成"
+echo "下载 argon_config ......" 
+/usr/bin/wget -P /tmp "${argon_config}"
+echo "安装 argon_config ......"
+opkg install luci-app-argon-config*.ipk && rm -f /tmp/luci-app-argon-config*.ipk && echo "安装 argon_config 完成"
