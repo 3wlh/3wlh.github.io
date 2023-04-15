9b0372423f176f18f40c0e4dbad7beb9889a902d
3wking <1094890624@qq.com>
2023-04-16 00:26:55 +0800

    更新 openclash.sh

--- OpenWrt/openclash.sh
+++ OpenWrt/openclash.sh
@@ -2,7 +2,6 @@
 PATH="/etc/openclash/core/"
 DEV="https://raw.iqiq.io/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
 META="https://raw.iqiq.io/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
-install{
 echo "内核安装中......"
 	if [ ! -d ${PATH} ]; then
 		mkdir ${PATH} && chmod 0755 ${PATH}
@@ -12,5 +11,3 @@ wget ${DEV} -O /tmp/dev.tar.gz && tar -zxf /tmp/dev.tar.gz -O > ${PATH}/clash &&
 echo "安装 meta 内核......" 
 wget ${METE} -O /tmp/meta.tar.gz && tar -zxf /tmp/meta.tar.gz  -O > ${PATH}/clash_meta && chmod 0755 ${PATH}/clash_meta && rm -f /tmp/meta*.gz
 echo "内核安装完成"
-}
-install
