2bd6cbc93b63fedad261ff33eaddac584f408b49
3wking <1094890624@qq.com>
2023-04-16 00:37:57 +0800

    更新 openclash.sh

--- OpenWrt/openclash.sh
+++ OpenWrt/openclash.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 PATH="/etc/openclash/core/"
 DEV="https://raw.iqiq.io/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
 META="https://raw.iqiq.io/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
@@ -7,6 +7,6 @@ echo "内核安装中......"
 		mkdir ${PATH} && chmod 0755 ${PATH}
 	fi
 echo "安装 dev 内核......" 
-wget "${DEV}" -O /tmp/dev.tar.gz && tar -zxf /tmp/dev.tar.gz -O > ${PATH}/clash && chmod 0755 ${PATH}/clash && rm -f /tmp/dev*.gz && echo "dev 内核安装完成"
+/usr/bin/wget "${DEV}" -O /tmp/dev.tar.gz && tar -zxf /tmp/dev.tar.gz -O > ${PATH}/clash && chmod 0755 ${PATH}/clash && rm -f /tmp/dev*.gz && echo "dev 内核安装完成"
 echo "安装 meta 内核......" 
-wget "${METE}" -O /tmp/meta.tar.gz && tar -zxf /tmp/meta.tar.gz  -O > ${PATH}/clash_meta && chmod 0755 ${PATH}/clash_meta && rm -f /tmp/meta*.gz && echo "meta 内核安装完成"
+/usr/bin/wget "${METE}" -O /tmp/meta.tar.gz && tar -zxf /tmp/meta.tar.gz  -O > ${PATH}/clash_meta && chmod 0755 ${PATH}/clash_meta && rm -f /tmp/meta*.gz && echo "meta 内核安装完成"
