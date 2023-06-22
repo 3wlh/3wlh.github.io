cc2c2f8345c42401f52fcd95dcbd9fe2817a2958
3wking <43069914+3wking@users.noreply.github.com>
2023-06-22 20:33:12 +0800

    up

--- frp/README.md
+++
@@ -1,6 +0,0 @@
-# FRP ：
-
-##### 安装frps服务端
-```sh
-wget https://3wking.github.io/frp/frps.sh -O frps.sh && chmod +x frps.sh && ./frps.sh
-```
--- frp/frps.sh
+++
@@ -1,9 +0,0 @@
-!#/usr/bin/sh
-wget https://3wking.github.io/frp/frps.tar.gz -O frps.tar.gz &&
-tar zxvf frps.tar.gz -C /usr/local &&
-mv -f /usr/local/frps/frps.service /usr/lib/systemd/system &&
-firewall-cmd --zone=public --add-port=7000/tcp --permanent &&
-firewall-cmd --zone=public --add-port=7001/tcp --permanent &&
-firewall-cmd --zone=public --add-port=80/tcp --permanent &&
-firewall-cmd --zone=public --add-port=443/tcp --permanent &&
-echo OK
