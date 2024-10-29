7a232e237ec86beb1582fc0d33668662488f24f8
3wlh <1094890624@qq.com>
2024-10-29 15:25:49 +0800

    更新 ping.js

--- ping.js
+++ ping.js
@@ -4,16 +4,11 @@ window.onload = function() {
 	const internalurl = document.getElementById("internal").innerText;
 	const error = document.getElementById("error").innerText;
 	let doname = /^([\w-]+\.)+((com)|(net)|(org)|(gov\.cn)|(info)|(cc)|(com\.cn)|(net\.cn)|(org\.cn)|(name)|(biz)|(tv)|(cn)|(mobi)|(name)|(sh)|(ac)|(io)|(tw)|(com\.tw)|(hk)|(com\.hk)|(ws)|(travel)|(us)|(tm)|(la)|(me\.uk)|(org\.uk)|(ltd\.uk)|(plc\.uk)|(in)|(eu)|(it)|(jp)|(top)|(ai))$/;
+	//let IP =/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/;
 	let flag_domain = doname.test(document.domain);
 	if(flag_domain){//是域名
 		// 域名
-		if(ping(externalurl)){
-			
-			window.location.replace(externalurl);
-			
-		}else{
-			window.location.replace(error);
-		};		
+		ping(externalurl)
 	}else{
 		// ip
 		//ping(externalurl)
@@ -27,6 +22,7 @@ window.onload = function() {
 		img.onload = function () {
 			flag = true;
 			console.log('ok');
+			window.location.replace(externalurl);
 			return true;
 			
 		};
@@ -34,14 +30,16 @@ window.onload = function() {
 			flag = true;
 			alert(externalurl)
 			console.log('ok');
+			window.location.replace(externalurl);
 			return true;
 		};
 		var timer = setTimeout(function () {
 			if (!flag) {    //如果真的无法访问
 				flag = false;
 				console.log('failed');
+				window.location.replace(error);
 				return false;
 			}
-		}, 1000);
+		}, 1500);
 	}
 }
