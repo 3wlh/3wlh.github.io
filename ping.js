adc875addc34ddb3f65a7307b7a8905f50d28483
3wlh <1094890624@qq.com>
2024-10-29 15:36:38 +0800

    更新 ping.js

--- ping.js
+++ ping.js
@@ -12,7 +12,7 @@ window.onload = function() {
 	}else{
 		// ip
 		//ping(externalurl)
-		//window.location.replace(internalurl);
+		window.location.replace(internalurl);
 		//alert(internalurl)
 	};
 	function ping (ip) {
@@ -22,7 +22,7 @@ window.onload = function() {
 		img.onload = function () {
 			flag = true;
 			console.log('ok');
-			//window.location.replace(externalurl);
+			window.location.replace(externalurl);
 			return true;
 			
 		};
@@ -30,14 +30,14 @@ window.onload = function() {
 			flag = true;
 			alert(externalurl)
 			console.log('ok');
-			//window.location.replace(externalurl);
+			window.location.replace(externalurl);
 			return true;
 		};
 		var timer = setTimeout(function () {
 			if (!flag) {    //如果真的无法访问
 				flag = false;
 				console.log('failed');
-				//window.location.replace(error);
+				window.location.replace(error);
 				return false;
 			}
 		}, 1500);
