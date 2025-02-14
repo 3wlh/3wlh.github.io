3efdc690152582dece9b6ab0a36908396b2c4ce0
3wlh <1094890624@qq.com>
2025-02-15 01:53:32 +0800

    up

--- version/update.php
+++ version/update.php
@@ -1,8 +1,8 @@
 <?php
     $password="*****";
-    $file=$_GET['file'];
-    $version=$_GET['version'];
-    $pwd=$_GET['pwd'];
+    $file=$_POST['file'];
+    $version=$_POST['version'];
+    $pwd=$_POST['pwd'];
     $DOCUMENT_ROOT=$_SERVER['DOCUMENT_ROOT'];
     //设置时区
     date_default_timezone_set('Asia/Shanghai');
@@ -21,11 +21,7 @@
 <?php
     echo '<p>提交中时间：'.$date.'</p>';
     echo '<p>当前运行脚本所在目录：'.$DOCUMENT_ROOT.'</p>';
-    //获取ip
-    if($pwd != $password ){
-        echo '<p>更新密码错误！</p>';
-        exit;
-    }
+    //判断数据
     if($file == null && $version == null ){
         echo '<p>您未更新.软件版本！</p>';
         exit;
@@ -33,10 +29,19 @@
         echo '<p>文件名称：'.$file.'.version</p>';
         echo '<p>软件版本：'.$version.'</p>';
     }
+    //判断密码
+    if($pwd != $password ){
+        echo '<p>更新密码错误！</p>';
+        exit;
+    }
     //设置文件输出内容和格式
     $out_put_string="$version";
     //打开文件,（写入文件）
-    @$fp=fopen("$file.version",'w');
+    $dir = './version';
+    if(!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    } 
+    @$fp=fopen("./version/$file.version",'w');
     flock($fp,LOCK_EX);
     if(!$fp){
         echo "<p><strong>软件版本.更新未完成！</strong></p></body></html>";
