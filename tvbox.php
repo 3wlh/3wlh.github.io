5c8f93ba9859db4367c2d184f0f63bbf2333898c
3wlh <1094890624@qq.com>
2025-02-18 21:41:31 +0800

    Update tvbox.php

--- tvbox.php
+++ tvbox.php
@@ -1,6 +1,11 @@
 <?php
-//重定向浏览器 
-header("Location:https://神器每日推送.tk/pz.json"); 
-//跳转至网址 
+// http://www.饭太硬.com/tv/
+// http://tv.nxog.top/m/
+// https://mpanso.me/DEMO.json
+// https://tv.xn--yhqu5zs87a.top
+// http://cdn.qiaoji8.com/tvbox.json
+// https://gh.19970101.xyz/https://raw.githubusercontent.com/yoursmile66/TVBox/main/XC.json
+header("Location:https://gh-proxy.com/raw.githubusercontent.com/yoursmile66/TVBox/main/XC.json"); 
 exit;
 ?>
+
